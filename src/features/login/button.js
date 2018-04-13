efcbf85619b08fc95c8f31a61092d498830037fb
Kongnut <kongnut.s@hotmail.com>
2018-04-13 22:04:27 +0700

    Fix Header

--- src/features/login/button.js
+++ src/features/login/button.js
@@ -15,7 +15,6 @@ class MyButton extends Component {
     var parent = this;
     //use this.props.userId to query password
     //and check condition if id and password match.
-<<<<<<< HEAD
     console.log("in");
     axios.post('http://localhost:8000/student_reg', {
     username: this.props.props.userId,
@@ -35,23 +34,6 @@ class MyButton extends Component {
   .catch(function (error) {
     console.log(error);
   });
-=======
-    axios
-      .post("http://localhost:8000/student_reg", {
-        username: this.props.props.userId,
-        password: this.props.props.password
-      })
-      .then(function(response) {
-        console.log(parent);
-        if (response.data.result == "success") {
-          console.log("in");
-          parent.props.props.onChange("loginSuccess", "true");
-        }
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
->>>>>>> 2f0cd693c22f7bc49be9e4c9b66607eb188e823f
 
     console.log(check);
     if (this.props.props.loginSuccess == "true") {
